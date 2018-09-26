90b4ae7238d92964bfdfb6bace749cc1b7e9f692
mathwiz <mathwiz@gmail.com>
2018-09-26 12:35:04 -0400

    decorator

--- groovy/src/pattern/structural/RoundedValueAsset.groovy
+++ groovy/src/pattern/structural/RoundedValueAsset.groovy
@@ -4,4 +4,9 @@ class RoundedValueAsset extends AssetDecorator {
     def value() {
         Math.round(asset .value())
     }
+
+    String toString() {
+        println("toString in RoundedValueAsset " + asset .getClass())
+        asset .toString()
+    }
 }
