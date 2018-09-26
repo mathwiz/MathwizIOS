a807a0d75d37b96f8cbe224280407bb726747c67
mathwiz <mathwiz@gmail.com>
2018-09-25 23:03:21 -0400

    decorator

--- groovy/src/pattern/structural/AssetDecorator.groovy
+++ groovy/src/pattern/structural/AssetDecorator.groovy
@@ -1,9 +1,31 @@
 package pattern.structural
 
-class AssetDecorator extends Asset {
+class AssetDecorator {
     def asset
 
-    def description() { asset .description() }
+    def value() {
+        asset .value()
+    }
+
+    def containsSecurity(aSecurity) {
+        asset .containsSecurity(aSecurity)
+    }
+
+    def description() {
+        asset .description()
+    }
+
+    def draw() {
+        asset .draw()
+    }
+
+    def depth() {
+        asset .depth()
+    }
+
+    def padding() {
+        asset .padding()
+    }
 
     String toString() {
         println("toString in AssetDecorator " + asset .getClass())
