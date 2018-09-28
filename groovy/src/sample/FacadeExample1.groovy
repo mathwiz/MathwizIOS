ca07702df7a9d869e15432020cc15cc82376cddf
mathwiz <mathwiz@gmail.com>
2018-09-27 23:08:47 -0400

    facade

--- groovy/src/sample/FacadeExample1.groovy
+++ groovy/src/sample/FacadeExample1.groovy
@@ -5,8 +5,8 @@ import pattern.structural.Player
 
 class FacadeExample1 extends CompositeExample1 {
     def doIt() {
-        def broker = new DatabaseBroker()
-        broker .save(new Player(name: "Tom Brady", pos: "qb", points: 127, salary: 3_000_000, isPersistent: false))
+        new DatabaseBroker()
+                .save(new Player(name: "Tom Brady", pos: "qb", points: 127, salary: 3_000_000, isPersistent: false))
     }
 
 }
