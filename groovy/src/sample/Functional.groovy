26e81fb4ca0cdef69e5dde0a58231acb055145f6
mathwiz <mathwiz@gmail.com>
2019-02-06 08:34:51 -0500

    Automata initial

--- groovy/src/sample/Functional.groovy
+++ groovy/src/sample/Functional.groovy
@@ -1,10 +1,18 @@
 package sample
 
 class Functional {
+    int step
+
     def inc(int n) { n + 1 }
+
+    def increase(int n) { n + step }
 
     static void main(String[] args) {
         def instance = new Functional()
-        [1, 2, 3, 4].collect({ instance.inc(it) }).each { println it }
+        [1, 2, 3, 4].collect({ instance.inc(it) }).each { print "${it} " }
+        println ""
+        def another = new Functional(step: 2)
+        [1, 2, 3, 4].collect({ another.increase(it) }).each { print "${it} " }
+        println ""
     }
 }
