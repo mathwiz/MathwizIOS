c3ef066372d72344f6c186c4fcdeb3f40de75c7a
mathwiz <mathwiz@gmail.com>
2019-02-07 22:46:49 -0500

    more automata

--- groovy/src/automata/Automaton.groovy
+++ groovy/src/automata/Automaton.groovy
@@ -30,5 +30,15 @@ class Automaton {
     private void makeCells(List<Byte> pattern) {
         row = new Cell[this.size]
         //recycle over init values
+        (0..(row.length - 1)).each {
+            row[it] = new Cell(color: pattern[it])
+            if (it == row.length - 1) {
+                row[it].setRight(row[0])
+                row[0].setLeft(row[it])
+            } else {
+                row[it].setLeft(row[it - 1])
+                row[it - 1].setRight(row[it])
+            }
+        }
     }
 }
--- groovy/src/automata/Console.groovy
+++ groovy/src/automata/Console.groovy
@@ -4,8 +4,13 @@ class Console {
     static final char mark = '*'
     static final char empty = ' '
 
+    String displayChar(val) {
+        (val == 0 ? empty : val) as String
+    }
+
     static void main(String[] args) {
-        println "${mark}${empty}${mark}"
+        Console c = new Console()
+        println "${c.displayChar(0)}${c.displayChar(2)}"
         println([] ? "true" : "false")
     }
 }
