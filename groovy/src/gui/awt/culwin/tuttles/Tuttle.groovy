e272b9396bfad629a74afe0123087fdb02f3c145
mathwiz <mathwiz@gmail.com>
2019-02-08 20:44:50 -0500

    Tuttles

--- groovy/src/gui/awt/culwin/tuttles/Tuttle.groovy
+++ groovy/src/gui/awt/culwin/tuttles/Tuttle.groovy
@@ -5,6 +5,7 @@ import java.awt.Canvas
 import java.awt.Color
 import java.awt.Graphics
 import java.awt.Image
+import java.awt.MediaTracker
 
 class Tuttle extends Canvas {
     private static final int SCREEN_STEPS = 500
@@ -51,7 +52,25 @@ class Tuttle extends Canvas {
     }
 
     void initializeCursor() {
+        MediaTracker tuttleTracker = new MediaTracker(this)
+        theCursor = myApplet.getImage(myApplet.getCodeBase(), "tuttle.gif")
+        tuttleTracker.addImage(theCursor, 0)
+        try {
+            tuttleTracker.waitForID(0)
+        } catch (Exception e) {
+            println e.toString()
+        }
 
+        if (theCursor == null ||
+                theCursor.getWidth(this) < 1 ||
+                theCursor.getHeight(this) < 1) {
+            println "Empty image...exiting"
+            System.exit(-1)
+        }
+
+        rotatingCursor = new TuttleCursor(theCursor, this)
+        rotatingCursor.setCursorColor(currentForeground)
+        theCursor = rotatingCursor.rotate(direction)
     }
 
     void update(Graphics g) {
@@ -66,31 +85,78 @@ class Tuttle extends Canvas {
     }
 
     void forward(int steps) {
+        int localDegrees = (direction + 270) % 360
+        double radians = localDegrees / 180 * Math.PI
+        int possibleNewX = xLocation + Math.round(Math.cos(radians) * steps)
+        int possibleNewY = yLocation + Math.round(Math.sin(radians) * steps)
 
+        if (possibleNewX >= -SCREEN_STEPS && possibleNewX <= SCREEN_STEPS &&
+                possibleNewY >= -SCREEN_STEPS && possibleNewY <= SCREEN_STEPS) {
+            int newX = Math.round(possibleNewX * horizontalScale)
+            int newY = Math.round(possibleNewY * verticalScale)
+            if (penDown) {
+                tuttleGraphics.drawLine(screenX, screenY, newX, newY)
+            }
+
+            xLocation = possibleNewX
+            yLocation = possibleNewY
+            screenX = newX
+            screenY = newY
+
+            repaint()
+        }
     }
 
     void backward(int steps) {
-
+        forward(-steps)
     }
 
     void turnRight(int degrees) {
-
+        direction += degrees
+        while (direction < 360) {
+            direction += 360
+        }
+        direction %= 360
+        theCursor = rotatingCursor.rotate(direction)
+        repaint()
     }
 
     void turnLeft(int degrees) {
-
+        turnRight(-degrees)
     }
 
     void clearTuttleArea() {
-
+        tuttleGraphics.setColor(currentBackground)
+        tuttleGraphics.fillRect(
+                -Math.floorDiv(screenWidth, 2),
+                -Math.floorDiv(screenHeight, 2),
+                screenWidth,
+                screenHeight)
+        tuttleGraphics.setColor(currentForeground)
+        repaint()
     }
 
     void resetTuttle() {
+        xLocation = 0
+        yLocation = 0
+        screenX = 0
+        screenY = 0
+        direction = 0
+        theCursor = rotatingCursor.rotate(direction)
+        repaint()
+    }
 
+    void clearAndReset() {
+        resetTuttle()
+        clearTuttleArea()
     }
 
     void setForeground(Color c) {
-
+        currentForeground = c
+        tuttleGraphics.setColor(c)
+        rotatingCursor.setCursorColor(c)
+        theCursor = rotatingCursor.rotate(direction)
+        repaint()
     }
 
     void setBackground(Color c) {
@@ -98,11 +164,11 @@ class Tuttle extends Canvas {
     }
 
     void setPenUp() {
-
+        penDown = false
     }
 
     void setPenDown() {
-
+        penDown = true
     }
 
     String getPenState() {
@@ -110,7 +176,7 @@ class Tuttle extends Canvas {
     }
 
     String getDetails() {
-        "X: ${xLocation}  Y: ${yLocation}  D: ${direction}  ${penState}"
+        "X: ${xLocation}  Y: ${yLocation * -1}  D: ${direction}  ${penState}" + " " + myApplet.getCodeBase()
     }
 
     private int calcCursorPos(int screenLocation) {
