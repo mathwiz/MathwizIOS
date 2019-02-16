25833b499fc6260a8540ffaf0f168eebad4a60ab
mathwiz <mathwiz@gmail.com>
2019-02-16 16:12:31 -0500

    fixed the init pattern behavior

--- groovy/src/automata/Console.groovy
+++ groovy/src/automata/Console.groovy
@@ -30,13 +30,25 @@ class Console {
         println ""
     }
 
-    List<Byte> randomPattern(int size) {
+    def randomPattern(int size) {
         List<Byte> pattern = []
         (0..(size - 1)).each {
-            byte val = (Math.random() > 0.5 ? 1 : 0) as byte
+            byte val = (Math.random() < 0.5 ? 0 : 1) as byte
             pattern.add(val)
         }
         pattern
+    }
+
+    def singlePattern(int size) {
+        List<Byte> pattern = []
+        (0..(size - 1)).each {
+            pattern.add(Math.floorDiv(size, 2) == it ? 1 : 0)
+        }
+        pattern
+    }
+
+    def initPattern(String pat) {
+        pat.collect { it as byte }
     }
 
     Rule getRule(int ruleNum) {
@@ -49,8 +61,10 @@ class Console {
         def size = (args.length > 0 ? args[0] : 40) as Integer
         def rows = (args.length > 1 ? args[1] : 40) as Integer
         def rule = args.length > 2 ? c.getRule(args[2] as Integer) : Rule.create(30)
-        def random = args.length > 3 ? args[3] != "0" : false
-        Automaton a = random ? new Automaton(rule, size, c.randomPattern(size)) : new Automaton(rule, size)
+        def pat = args.length > 3 ? args[3] : null
+        def random = pat == "r"
+        def init = !random && pat != null ? c.initPattern(pat) : c.singlePattern(size)
+        Automaton a = random ? new Automaton(rule, size, c.randomPattern(size)) : new Automaton(rule, size, init)
         c.displayHeader(a)
         c.displayRow(a)
         (0..rows).each {
