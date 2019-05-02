e32a875c08ce33418f85b6aa346d323796df5b7a
Yohan Lee <mathwiz@gmail.com>
2019-05-02 14:12:04 -0400

    Dialogs

--- groovy/src/gui/awt/culwin/ch6/VersionDialog.groovy
+++ groovy/src/gui/awt/culwin/ch6/VersionDialog.groovy
@@ -1,4 +1,18 @@
 package gui.awt.culwin.ch6
 
-class VersionDialog {
+import java.awt.Dialog
+import java.awt.Frame
+import java.awt.event.ActionEvent
+import java.awt.event.ActionListener
+
+class VersionDialog extends Dialog implements ActionListener {
+
+    VersionDialog(Frame parentFrame, String message) {
+        super(parentFrame, "Version", false)
+    }
+
+    @Override
+    void actionPerformed(ActionEvent actionEvent) {
+
+    }
 }
