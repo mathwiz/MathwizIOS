262f354d448eba75262f8566fabd77a95cd7b1dd
Yohan Lee <mathwiz@gmail.com>
2019-05-02 10:16:16 -0400

    Dialog

--- groovy/src/gui/awt/culwin/ch6/ExitDialog.groovy
+++ groovy/src/gui/awt/culwin/ch6/ExitDialog.groovy
@@ -2,11 +2,13 @@ package gui.awt.culwin.ch6
 
 import gui.awt.culwin.MessageCanvas
 
-import javax.swing.Action
 import java.awt.Button
+import java.awt.Color
 import java.awt.Dialog
+import java.awt.Dimension
 import java.awt.Frame
 import java.awt.Panel
+import java.awt.Point
 import java.awt.Window
 import java.awt.event.ActionEvent
 import java.awt.event.ActionListener
@@ -26,6 +28,32 @@ class ExitDialog extends Dialog implements ActionListener {
         myListener = listener
 
         messageCanvas = new MessageCanvas(("Are you sure\n you want to exit?"))
+        messageCanvas.setBackground(Color.WHITE)
+
+        yesButton = new Button("yes")
+        yesButton.setActionCommand("yes")
+        yesButton.addActionListener(this)
+        buttonPanel.add(yesButton)
+
+        noButton = new Button("no")
+        noButton.setActionCommand("no")
+        noButton.addActionListener(this)
+        buttonPanel.add(noButton)
+
+        this.add(messageCanvas, "Center")
+        this.add(buttonPanel, "South")
+        this.pack()
+    }
+
+    void setVisible(boolean showIt) {
+        Point loc = new Point()
+        Point parentLoc = parentWindow.getLocationOnScreen()
+        Dimension parentSize = parentWindow.getSize()
+        if (showIt) {
+            loc.x = parentLoc.x + parentSize.width/2 - this.getSize().width/2
+            loc.y = parentLoc.y + parentSize.height/2 - this.getSize().height/2
+        }
+        super.setVisible(showIt)
     }
 
     @Override
