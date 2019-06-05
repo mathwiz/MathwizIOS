219bec10c5ec26db6f14286b707f14be0bb44f55
Yohan Lee <mathwiz@gmail.com>
2019-06-05 13:31:35 -0400

    Text interface

---
+++ groovy/src/gui/awt/culwin/tuttles/TextTuttle.groovy
@@ -0,0 +1,10 @@
+package gui.awt.culwin.tuttles
+
+import gui.awt.culwin.tuttles.Tuttle
+import java.applet.Applet
+
+class TextTuttle extends Tuttle {
+    TextTuttle(Applet applet, int width, int height) {
+        super(applet, width, height)
+    }
+}
