296be124f2494513fad815d4f2c2a0b40eaca3de
Yohan Lee <mathwiz@gmail.com>
2019-06-06 11:49:33 -0400

    Text interface

--- groovy/src/gui/awt/culwin/ch7/TextMenuTuttleInterface.groovy
+++ groovy/src/gui/awt/culwin/ch7/TextMenuTuttleInterface.groovy
@@ -1,11 +1,8 @@
 package gui.awt.culwin.ch7
 
-import gui.awt.culwin.tuttles.TuttleButton
 
 import java.applet.Applet
 import java.awt.*
-import java.awt.event.ActionListener
-import java.util.List
 
 class TextMenuTuttleInterface extends Panel {
     static final int TOP_LEVEL_MENU = 0
@@ -23,6 +20,20 @@ class TextMenuTuttleInterface extends Panel {
     static final int HELP_MENU = 12
     static final int EXIT_MENU = 13
 
+    private static final String topLevelMenu = "Move Turn Colors Pen Screen Help Exit"
+    private static final String moveMenu = "\n\tMove: Forwards Backwards"
+    private static final String moveForwardsMenu = "\n\t\tMove Forwards: 5 10 25"
+    private static final String moveBackwardsMenu = "\n\t\tMove Backwards: 5 10 25"
+    private static final String turnMenu = "\n\tTurn: Left Right"
+    private static final String turnLeftMenu = "\n\t\tTurn Left: 5 45 90"
+    private static final String turnRightMenu = "\n\t\tTurn Right: 5 45 90"
+    private static final String colorMenu = "\n\tColors: Foreground Background"
+    private static final String colorForegroundMenu = "\n\t\tColors Foreground: Black White Red Green Yellow Blue"
+    private static final String colorBackgroundMenu = "\n\t\tColors Background: Black White Red Green Yellow Blue"
+    private static final String penMenu = "\n\tPen: Up Down"
+    private static final String screenMenu = "\n\tSCreen: Clear Reset ClearAndReset"
+    private static final String helpMenu = "\n\tHelp; Help Version"
+    private static final String exitMenu = "\n\tExit: Yes No"
 
     private int menuState = TOP_LEVEL_MENU
 
@@ -39,6 +50,53 @@ class TextMenuTuttleInterface extends Panel {
 
     void setMenuState(int newState) {
         menuState = newState
+        switch (menuState) {
+            case TOP_LEVEL_MENU:
+                menuArea.setText(topLevelMenu)
+                break
+            case MOVE_MENU:
+                menuArea.setText(topLevelMenu + moveMenu)
+                break
+            case MOVE_BACKWARD_MENU:
+                menuArea.setText(topLevelMenu + moveMenu + moveBackwardsMenu)
+                break
+            case MOVE_FOREWARD_MENU:
+                menuArea.setText(topLevelMenu + moveMenu + moveForwardsMenu)
+                break
+            case TURN_MENU:
+                menuArea.setText(topLevelMenu + turnMenu)
+                break
+            case TURN_LEFT_MENU:
+                menuArea.setText(topLevelMenu + turnMenu + turnLeftMenu)
+                break
+            case TURN_RIGHT_MENU:
+                menuArea.setText(topLevelMenu + turnMenu + turnRightMenu)
+                break
+            case COLOR_MENU:
+                menuArea.setText(topLevelMenu + colorMenu)
+                break
+            case FOREGROUND_COLOR_MENU:
+                menuArea.setText(topLevelMenu + colorMenu + colorForegroundMenu)
+                break
+            case BACKGROUND_COLOR_MENU:
+                menuArea.setText(topLevelMenu + colorMenu + colorBackgroundMenu)
+                break
+            case PEN_MENU:
+                menuArea.setText(topLevelMenu + penMenu)
+                break
+            case SCREEN_MENU:
+                menuArea.setText(topLevelMenu + screenMenu)
+                break
+            case HELP_MENU:
+                menuArea.setText(topLevelMenu + helpMenu)
+                break
+            case EXIT_MENU:
+                menuArea.setText(topLevelMenu + exitMenu)
+                break
+        }
+    }
 
+    int enuStateIs() {
+        return menuState
     }
 }
