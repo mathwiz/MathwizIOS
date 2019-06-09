5debd44f36b1d4b81a07809a75a5e354d09278d1
Yohan Lee <mathwiz@gmail.com>
2019-06-09 14:06:24 -0400

    Undo

---
+++ groovy/src/gui/awt/culwin/ch8/UndoingCommandLineTuttle.groovy
@@ -0,0 +1,145 @@
+package gui.awt.culwin.ch8
+
+
+import gui.awt.culwin.ch7.CommandLineTuttleInterface
+import gui.awt.culwin.tuttles.BufferedTuttle
+import gui.awt.culwin.tuttles.TextTuttle
+
+import java.applet.Applet
+import java.awt.*
+import java.awt.event.ActionEvent
+import java.awt.event.ActionListener
+
+class UndoingCommandLineTuttle extends Applet implements ActionListener {
+    BufferedTuttle theTuttle
+    Label feedbackLabel = new Label()
+    Panel feedbackPanel = new Panel()
+    Panel tuttlePanel = new Panel()
+    CommandLineTuttleInterface theInterface
+
+    void init() {
+        this.setLayout(new BorderLayout())
+        this.setFont(new Font("TimesRoman", Font.PLAIN, 14))
+        this.setBackground(Color.WHITE)
+
+        feedbackPanel.add(feedbackLabel)
+
+        theTuttle = new BufferedTuttle(this, 500, 500)
+        tuttlePanel.add(theTuttle)
+
+        theInterface = new CommandLineTuttleInterface(this)
+
+        this.add(feedbackPanel, "North")
+        this.add(tuttlePanel, "Center")
+        this.add(theInterface, "South")
+
+        this.feedback()
+        theInterface.clearCommandArea()
+    }
+
+    def feedback() {
+        feedbackLabel.setText(theTuttle.getDetails())
+        feedbackPanel.doLayout()
+    }
+
+
+    @Override
+    void actionPerformed(ActionEvent event) {
+        def cmd = event.getActionCommand()
+        println "Action: ${cmd}"
+
+        def (first, second) = cmd.tokenize(" ")
+
+        def resp = ""
+        switch (first) {
+            case "help":
+                resp = obtainHelp(second)
+                break
+            case "exit":
+                resp = checkExit(second)
+                break
+            default:
+
+                resp = theTuttle.doCommand(cmd)
+        }
+
+        theInterface.clearCommandArea()
+        theInterface.appendFeedback("\n> ${cmd}")
+        if (resp) {
+            theInterface.appendFeedback("\n${resp}")
+        }
+
+        this.feedback()
+    }
+
+    def obtainHelp(String what) {
+        def theHelp = ""
+
+        if (what) {
+            def helpFor = theTuttle.identifyCommand(what.toLowerCase())
+            switch (helpFor) {
+                case TextTuttle.FORWARD:
+                    theHelp += "fd is Forward, followed by a number"
+                    break
+                case TextTuttle.BACKWARD:
+                    theHelp += "bd is Backward, followed by a number"
+                    break
+                case TextTuttle.TURN_LEFT:
+                    theHelp += "tl is Turn Left, followed by a number"
+                    break
+                case TextTuttle.TURN_RIGHT:
+                    theHelp += "tr is Turn Right, followed by a number"
+                    break
+                case TextTuttle.BACKGROUND:
+                    theHelp += "bg is Change Background, followed by a color"
+                    break
+                case TextTuttle.FOREGROUND:
+                    theHelp += "fg is Change Foreground, followed by a color"
+                    break
+                case TextTuttle.PEN_DOWN:
+                    theHelp += "pd is Pen Down"
+                    break
+                case TextTuttle.PEN_UP:
+                    theHelp += "pu is Pen Up"
+                    break
+                case TextTuttle.CLEAR:
+                    theHelp += "cl is Clear"
+                    break
+                case TextTuttle.CLEAR_AND_RESET:
+                    theHelp += "cr is Clear and Reset"
+                    break
+                case TextTuttle.RESET:
+                    theHelp += "rs is Reset"
+                    break
+                case TextTuttle.EXIT:
+                    theHelp += "Use 'exit please'"
+                    break
+                case BufferedTuttle.UNDO:
+                    theHelp += "undo reverses the last command"
+                default:
+                    theHelp += "Sorry that command is not valid."
+            }
+        } else {
+            theHelp += "help is available for these commands: ${TextTuttle.commands}"
+        }
+        theHelp
+    }
+
+    def checkExit(String term) {
+        if (term == "please") {
+            System.exit(0)
+        }
+        "Use 'exit please' to quit"
+    }
+
+    static void main(String[] args) {
+        def frame = new Frame("Demonstration")
+        def theInterface = new UndoingCommandLineTuttle()
+
+        theInterface.init()
+        frame.add(theInterface, "Center")
+
+        frame.setVisible(true)
+        frame.setSize(frame.getPreferredSize())
+    }
+}
--- groovy/src/gui/awt/culwin/tuttles/BufferedTuttle.groovy
+++ groovy/src/gui/awt/culwin/tuttles/BufferedTuttle.groovy
@@ -5,55 +5,161 @@ import java.awt.*
 
 class BufferedTuttle extends TextTuttle {
     static final int UNKNOWN = -1
-    static final int FORWARD = 0
-    static final int BACKWARD = 1
-    static final int TURN_RIGHT = 2
-    static final int TURN_LEFT = 3
-    static final int FOREGROUND = 4
-    static final int BACKGROUND = 5
-    static final int PEN_UP = 6
-    static final int PEN_DOWN = 7
     static final int CLEAR = 8
     static final int RESET = 9
     static final int CLEAR_AND_RESET = 10
     static final int EXIT = 11
-    static final int MAX_COMMANDS = 11
+    static final int UNDO = 12
+    static final int LOAD = 13
+    static final int SAVE = 14
+    static final int MAX_COMMANDS = 15
 
-    public static final String[] commands = [
-            "fd", "bd", "tr", "tl",
-            "fg", "bg", "pu", "pd",
-            "cl", "rs", "cr", "exit"
-    ]
-
-    String messageState
+    private boolean tuttleShowing
+    private int startX
+    private int startY
+    private int startDirection
+    private Color startForeground
+    private Color startBackground
+    private boolean startPenStatus
+    private final def commandBuffer = []
 
     BufferedTuttle(Applet applet, int width, int height) {
         super(applet, width, height)
+        this.storeTuttleStatus()
+        this.showTuttle()
     }
 
-    String getDetails() {
-        super.getDetails() + " Memo: ${messageState}"
+    @Override
+    void update(Graphics g) {
+        if (tuttleShowing) {
+            super.update(g)
+        }
+    }
+
+    def tuttleLocationIs() {
+        new Point(xLocation, yLocation)
+    }
+
+    def tuttleDirectionIs() {
+        this.direction
+    }
+
+    def tuttleForegroundIs() {
+        this.currentForeground
+    }
+
+    def tuttleBackgroundIs() {
+        this.currentBackground
+    }
+
+    def tuttlePenStatusIs() {
+        this.penDown
+    }
+
+    def isUndoAvailable() {
+        !commandBuffer.isEmpty()
+    }
+
+    def whatUndoIsAvailable() {
+        commandBuffer.join(" ")
+    }
+
+    def clearAndReset(Color c) {
+        setBackground(c)
+        clearAndReset()
+    }
+
+    def hideTuttle() {
+        tuttleShowing = false
+    }
+
+    def showTuttle() {
+        tuttleShowing = true
+    }
+
+    def undo() {
+        String reply = ""
+        int numCommands = commandBuffer.size()
+        if (numCommands > 0) {
+            def last = commandBuffer.pop()
+            reply += "undo " + last
+            hideTuttle()
+            restoreTuttleStatus()
+            for (int it = 0; it < commandBuffer.size(); it++) {
+                super.doCommand(commandBuffer[it])
+            }
+            showTuttle()
+        } else {
+            reply += "Nothing to undo!"
+        }
+        reply
+    }
+
+    def save(StringTokenizer what) {
+        println "save called"
+    }
+
+    def load(StringTokenizer what) {
+        println "load called"
+    }
+
+    def storeTuttleStatus() {
+        def p = tuttleLocationIs()
+        startX = p.x
+        startY = p.y
+        startDirection = tuttleDirectionIs()
+        startForeground = tuttleForegroundIs()
+        startBackground = tuttleBackgroundIs()
+        startPenStatus = tuttlePenStatusIs()
+        commandBuffer.removeAll()
+    }
+
+    def restoreTuttleStatus() {
+        setForeground(startForeground)
+        clearAndReset(startBackground)
+        setPenUp()
+        turnRight(90)
+        forward(startX)
+        turnLeft(90)
+        forward(startY)
+        turnRight(startDirection)
+        if (startPenStatus) {
+            setPenDown()
+        } else {
+            setPenUp()
+        }
     }
 
     String doCommand(String theCommand) {
-        println "Received command ${theCommand}"
         StringTokenizer tokenizer = new StringTokenizer(theCommand)
-        String firstTerm
         String reply
-        int thisCommand
+        int thisCommand = UNKNOWN
 
         if (tokenizer.hasMoreTokens()) {
-            firstTerm = tokenizer.nextToken().toLowerCase()
+            String firstTerm = tokenizer.nextToken().toLowerCase()
             thisCommand = identifyCommand(firstTerm)
             println "Identified command ${thisCommand}"
 
-            if (thisCommand == UNKNOWN) {
-                reply = "The command ${firstTerm} is not known!"
+            if (thisCommand == UNDO) {
+                reply = undo()
+            } else if (thisCommand == SAVE) {
+                reply = save(tokenizer)
+            } else if (thisCommand == LOAD) {
+                reply = load(tokenizer)
             } else {
-                reply = dispatchCommand(thisCommand, tokenizer)
+                reply = super.doCommand(theCommand)
             }
         } else {
             reply = "There does not seem to be a command given!"
+        }
+
+        if (!reply) {
+            if (thisCommand == CLEAR_AND_RESET || thisCommand == CLEAR) {
+                storeTuttleStatus()
+            } else if (thisCommand != CLEAR && thisCommand != CLEAR_AND_RESET
+                    && thisCommand != SAVE && thisCommand != LOAD) {
+                commandBuffer.add(theCommand)
+            }
         }
 
         messageState = reply
@@ -61,130 +167,14 @@ class BufferedTuttle extends TextTuttle {
     }
 
     int identifyCommand(String cmd) {
-        println "Identifying ${cmd}"
-        int thisCommand = MAX_COMMANDS
-        int identified = UNKNOWN
-        while (identified == UNKNOWN && thisCommand != UNKNOWN) {
-            if (cmd == commands[thisCommand]) {
-                identified = thisCommand
-            } else {
-                thisCommand--
-            }
+        if (cmd == "undo") {
+            return UNDO
+        } else if (cmd == "save") {
+            return SAVE
+        } else if (cmd == "load") {
+            return LOAD
+        } else {
+            return super.identifyCommand(cmd)
         }
-        println "Identified command ${identified}"
-        identified
-    }
-
-    def dispatchCommand(int commandId, StringTokenizer arguments) {
-        def resp = ""
-        def processed = false
-        println "dispatchCommand received: ${commandId}"
-        switch (commandId) {
-            case FORWARD:
-            case BACKWARD:
-            case TURN_LEFT:
-            case TURN_RIGHT:
-                if (arguments.countTokens() == 1) {
-                    int toStepOrTurn
-                    try {
-                        toStepOrTurn = Integer.parseInt(arguments.nextToken())
-                        println "toSTepOrTurn ${toStepOrTurn}"
-                        switch (commandId) {
-                            case FORWARD:
-                                this.forward(toStepOrTurn)
-                                break
-                            case BACKWARD:
-                                this.backward(toStepOrTurn)
-                                break
-                            case TURN_LEFT:
-                                this.turnLeft(toStepOrTurn)
-                                break
-                            case TURN_RIGHT:
-                                this.turnRight(toStepOrTurn)
-                                break
-                        }
-                        processed = true
-                    } catch (NumberFormatException e) {
-                        processed = false
-                    }
-                }
-                if (!processed) {
-                    resp += commands[commandId]
-                    resp += (" should be followed by a single number.")
-                }
-                break
-            case PEN_DOWN:
-            case PEN_UP:
-            case RESET:
-            case CLEAR:
-            case CLEAR_AND_RESET:
-                if (arguments.countTokens() == 0) {
-                    switch (commandId) {
-                        case PEN_UP:
-                            this.setPenUp()
-                            break
-                        case PEN_DOWN:
-                            this.setPenDown()
-                            break
-                        case RESET:
-                            this.resetTuttle()
-                            break
-                        case CLEAR:
-                            this.clearTuttleArea()
-                            break
-                        case CLEAR_AND_RESET:
-                            this.clearAndReset()
-                            break
-                    }
-                    processed = true
-                }
-                if (!processed) {
-                    resp += (commands[commandId])
-                    resp += (" should not be followed by anything.")
-                }
-                break
-            case FOREGROUND:
-            case BACKGROUND:
-                if (arguments.countTokens() == 1) {
-                    Color c = identifyColor(arguments.nextToken().toLowerCase())
-                    if (c != null) {
-                        if (commandId == FOREGROUND) {
-                            this.setForeground(c)
-                        } else {
-                            this.setBackground(c)
-                        }
-                        processed = true
-                    }
-                }
-                if (!processed) {
-                    resp += (commands[commandId])
-                    resp += (" should only be followed by")
-                    resp += (" white, black, red, blue, green, or yellow.")
-                }
-                break
-            default:
-                resp += ("What?")
-        }
-
-        println "dispatchCommand returning message ${resp}"
-        resp
-    }
-
-    Color identifyColor(String color) {
-        Color c = null
-        if (color == "black") {
-            c = Color.BLACK
-        } else if (color == "white") {
-            c = Color.WHITE
-        } else if (color == "yellow") {
-            c = Color.YELLOW
-        } else if (color == "green") {
-            c = Color.GREEN
-        } else if (color == "red") {
-            c = Color.RED
-        } else if (color == "blue") {
-            c = Color.BLUE
-        }
-        return c
     }
 }
