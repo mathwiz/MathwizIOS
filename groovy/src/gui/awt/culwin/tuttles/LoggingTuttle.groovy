839faa3f6a84514a6797321127c1b372eb35d696
Yohan Lee <mathwiz@gmail.com>
2019-06-10 16:34:59 -0400

    Logging

--- groovy/src/gui/awt/culwin/tuttles/LoggingTuttle.groovy
+++ groovy/src/gui/awt/culwin/tuttles/LoggingTuttle.groovy
@@ -11,4 +11,38 @@ class LoggingTuttle extends BufferedTuttle {
         super(applet, width, height)
         this.addMouseListener(listener)
     }
+
+    String doCommand(String theCommand) {
+        logCommand(theCommand)
+        super.doCommand(theCommand)
+    }
+
+    void logCommand(String theCommand) {
+        if (loggingActive) {
+            TimeStamp stamp = new TimeStamp(true)
+            logFile.println("${stamp} ${theCommand}")
+        }
+    }
+
+    class TimeStamp {
+        private int theStamp
+
+        TimeStamp() {
+            this.stamp()
+        }
+
+        TimeStamp(boolean toStampNow) {
+            if (toStampNow) {
+                this.stamp()
+            }
+        }
+
+        void stamp() {
+
+        }
+
+        String toString() {
+
+        }
+    }
 }
