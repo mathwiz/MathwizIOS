5a06ae82c521c7bc98eecb9a21e38ebd2f2255d0
Yohan Lee <mathwiz@gmail.com>
2019-06-10 14:24:00 -0400

    File dialog

--- groovy/src/gui/awt/culwin/ch8/TuttleOpenDialog.groovy
+++ groovy/src/gui/awt/culwin/ch8/TuttleOpenDialog.groovy
@@ -5,11 +5,15 @@ import java.awt.FileDialog
 import java.awt.Frame
 import java.awt.Point
 import java.awt.Window
+import java.awt.event.ActionEvent
 import java.awt.event.ActionListener
 
 class TuttleOpenDialog extends FileDialog {
     protected Window itsParentWindow
     protected ActionListener itsListener
+
+    private String openFilename
+    private String openDirname
 
     TuttleOpenDialog(Frame parentFrame, ActionListener listener) {
         super(parentFrame, "Tuttle Open", FileDialog.LOAD)
@@ -28,8 +32,38 @@ class TuttleOpenDialog extends FileDialog {
             itsLocation.x = itsParentsLocation.x + itsParentsSize.width/2 - itsSize.width/2
             itsLocation.y = itsParentsLocation.y + itsParentsSize.height/2 - itsSize.height/2
             setLocation(itsLocation)
+
+            setDirectory("")
+            setFile("")
+            super.setVisible(true)
+
+            String tempDirname = getDirectory()
+            if (tempDirname) {
+                openDirname = tempDirname
+            }
+
+            String tempFilename = getFile()
+            if (tempFilename) {
+                openFilename = tempFilename
+                itsListener.actionPerformed(new ActionEvent(this, ActionEvent.ACTION_PERFORMED, "loadit"))
+            }
         }
-        super.setVisible(showIt)
+        super.setVisible(false)
     }
 
+    boolean isFilenameAvailable() {
+        openFilename != null
+    }
+
+    String filenameIs() {
+        openFilename
+    }
+
+    String dirnameIs() {
+        openDirname
+    }
+
+    String fullFilenameIs() {
+        "${openDirname}/${openDirname}"
+    }
 }
